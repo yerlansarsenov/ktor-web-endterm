5b663fba87503176b255040a5ad5237adf09b074
Yerlan Sarsenov <erlan@1fit.app>
2023-12-16 15:31:17 +0600

    add models

---
+++ src/dto/TodoDTO.kt
@@ -0,0 +1,7 @@
+package com.webendterm.dto
+
+data class TodoDTO(
+    val id: String? = null,
+    val title: String? = null,
+    val done: Boolean = false
+)
---
+++ src/model/Todo.kt
@@ -0,0 +1,6 @@
+package com.webendterm.model
+
+data class Todo(
+    val id: String?,
+    val title: String
+)
